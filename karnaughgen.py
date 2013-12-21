79e16849591b7333fb800657a6f6c71de6fc8e9a
Linus Karlsson <linus@zozs.se>
2013-12-21 16:30:47 +0100

    Add validation of selection.
     * The selection is parsed and verified, and a cube is calculated.
     * A list of implicants is added, where the implicants will be added.

--- karnaughgen.py
+++ karnaughgen.py
@@ -13,7 +13,7 @@
 #    this list of conditions and the following disclaimer in the documentation
 #    and/or other materials provided with the distribution.
 #
-#  * Neither the name of Linus Karlsson nor the names of its contributors may
+#  * Neither the name of Linus Karlsson nor the names of the contributors may
 #    be used to endorse or promote products derived from this software without
 #    specific prior written permission.
 #
@@ -40,16 +40,22 @@ class MainFrame(QtGui.QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.resize(400, 400)
+        self.resize(500, 300)
+        self.move(50, 50)
         self.setWindowTitle('KarnaughLaTeX generator')
-        self._vbox = QtGui.QVBoxLayout()
-        self._vbox.setContentsMargins(0, 0, 0, 0)
+        self._hbox = QtGui.QHBoxLayout()
+        self._hbox.setContentsMargins(0, 0, 0, 0)
         # Karnaugh map
         self._karnaugh_map = KarnaughMap(4)
-        self._vbox.addWidget(self._karnaugh_map, 1)
+        self._hbox.addWidget(self._karnaugh_map)
+        # Implicant list
+        self._implicant_list = ImplicantList()
+        self._hbox.addWidget(self._implicant_list, QtCore.Qt.AlignRight)
         # Add implicant-button
+        self._vbox = QtGui.QVBoxLayout()
         self._add_button = QtGui.QPushButton('Add implicant')
         self._add_button.clicked.connect(self._add_implicant)
+        self._vbox.addLayout(self._hbox, 1)
         self._vbox.addWidget(self._add_button)
         # Cannot set layout on QMainWindow, so placeholder widget must be made.
         layout_widget = QtGui.QWidget()
@@ -57,7 +63,40 @@ class MainFrame(QtGui.QMainWindow):
         self.setCentralWidget(layout_widget)
 
     def _add_implicant(self):
-        print("Clicked!")
+        # Fetch current selection, validate it, convert to a cube ({0, 1, B})
+        vertices = self._karnaugh_map.selected_vertices()
+        cube = self._validate_selection(vertices)
+        # Add cube to implicant list.
+        self._implicant_list.addItem(cube)
+
+    def _validate_selection(self, vertices):
+        def is_power_of_two(num):
+            return num != 0 and (num & (num - 1)) == 0
+        # Check total item count. Must be a power of 2.
+        if not is_power_of_two(len(vertices)):
+            raise Exception("Total count not power of 2, was", len(vertices))
+        # Ensures that selection is a rectangle with element count that is 2^x.
+        return self._vertices_to_cube(vertices)
+
+    def _vertices_to_cube(self, vertices):
+        """Convert the list of vertices to a cube.
+
+        Throw an exception if the calculated cube has a size that does not
+        equal the coordinate count. This may happen if the input vertices
+        is not a rectangular area in the Karnaugh map.
+        """
+        def lattice(var):
+            if all(i == '1' for i in var):
+                return '1'
+            elif all(i == '0' for i in var):
+                return '0'
+            else:
+                return 'B'
+        cube = ''.join(lattice(var) for var in zip(*vertices))
+        # Now ensure that the generated cube has size equal to the vertex count
+        if 2 ** cube.count('B') != len(vertices):
+            raise Exception("Invalid implicant selection for cube", cube)
+        return cube
 
 
 class KarnaughMap(QtGui.QTableWidget):
@@ -83,12 +122,25 @@ class KarnaughMap(QtGui.QTableWidget):
         for col in range(0, top_vars):
             self.setColumnWidth(col, self.rowHeight(0))
 
+    def selected_vertices(self):
+        """Returns a set of vertices of the selected items."""
+        coords = ((i.row(), i.column()) for i in self.selectedIndexes())
+        return [self._left_values[r] + self._top_values[c] for r, c in coords]
+
     def _set_headers(self, left_vars, top_vars):
         # left_vars and top_vars will only be 2 or 4.
-        def set_header(vars, header_func):
-            header_func(['0', '1'] if vars == 2 else ['00', '01', '11', '10'])
-        set_header(left_vars, self.setVerticalHeaderLabels)
-        set_header(top_vars, self.setHorizontalHeaderLabels)
+        two_var = ['0', '1']
+        four_var = ['00', '01', '11', '10']
+        self._left_values = two_var if left_vars == 2 else four_var
+        self._top_values = two_var if top_vars == 2 else four_var
+        self.setVerticalHeaderLabels(self._left_values)
+        self.setHorizontalHeaderLabels(self._top_values)
+
+
+class ImplicantList(QtGui.QListWidget):
+
+    def __init__(self):
+        super().__init__()
 
 
 def main():
